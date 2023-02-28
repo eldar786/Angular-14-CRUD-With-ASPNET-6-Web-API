31d40c1508147812bb27a25272c1805479abca2a
eldar.bajrovic <eldar.bajrovic@gmail.com>
2023-02-28 15:15:03 +0100

    First commit

---
+++ FullStack API/FullStack.API/FullStack.API/Data/FullStackDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FullStack.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FullStack.API.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
